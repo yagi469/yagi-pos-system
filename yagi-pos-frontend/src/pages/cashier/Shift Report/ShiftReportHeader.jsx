de82217183b60b4eaf9b23be1073c89591302bb3
yaginumatti <ryohei122yagi@gmail.com>
2025-11-30 21:40:54 +0900

    feat: add ShiftReportHeader component for cashier shift summary UI.

--- yagi-pos-frontend/src/pages/cashier/Shift Report/ShiftReportHeader.jsx
+++ yagi-pos-frontend/src/pages/cashier/Shift Report/ShiftReportHeader.jsx
@@ -1,9 +1,20 @@
+import { ArrowRight } from "lucide-react";
 import React from "react";
+import { Button } from "@/components/ui/button";
+
 
 const ShiftReportHeader = () => {
     return (
-        <div>
-            <h1>Shift Report Header</h1>
+        <div className="p-4 bg-card border-b">
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl font-bold">Shift Summary</h1>
+                <div className="flex gap-2">
+                    <Button variant={"destructive"}>
+                        <ArrowRight />
+                        End Shift & Logout
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 };
