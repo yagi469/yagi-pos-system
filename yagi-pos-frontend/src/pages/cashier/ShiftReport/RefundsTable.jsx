139a03f1c3f782f4f3c00861c5b3f2149c20b300
yaginumatti <ryohei122yagi@gmail.com>
2025-12-02 02:06:25 +0900

    feat: add RefundsTable component to display refund details in the shift report.

--- yagi-pos-frontend/src/pages/cashier/ShiftReport/RefundsTable.jsx
+++ yagi-pos-frontend/src/pages/cashier/ShiftReport/RefundsTable.jsx
@@ -1,10 +1,57 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { TableBody, Table, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import React from "react";
+
+const shiftData = {
+    refunds: [
+        {
+            id: 234,
+            orderId: "2",
+            reason: "wrong product received",
+            amount: 699
+        },
+        {
+            id: 2,
+            orderId: "2",
+            reason: "wrong product received",
+            amount: 14899,
+        },
+        {
+            id: 3,
+            orderId: "2",
+            reason: "wrong product received",
+            totalAmount: 1200,
+        }
+    ]
+}
 
 const RefundsTable = () => {
     return (
-        <div>
-            <h1>Refunds Table</h1>
-        </div>
+        <Card>
+            <CardContent>
+                <h2 className="text-xl font-semibold mb-4">Refund Processed</h2>
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[150px]">Refund ID</TableHead>
+                            <TableHead className="w-[150px]">Order ID</TableHead>
+                            <TableHead className="w-[150px]">Reason</TableHead>
+                            <TableHead className="text-right">Amount</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {shiftData.refunds.map((refund) => (
+                            <TableRow key={refund.id}>
+                                <TableCell>RFD - {refund.id}</TableCell>
+                                <TableCell>ORD - {refund.orderId}</TableCell>
+                                <TableCell>{refund.reason}</TableCell>
+                                <TableCell className="text-right">₹{refund.amount}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
     );
 };
 
